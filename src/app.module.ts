c14c811f82e462807d5319daa0566825a2f26a98
Julien <julien.genel@gmail.com>
2022-06-11 18:10:12 +0200

    ajout protection routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,20 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TodoModule } from './todo/todo.module';
 import { TaskModule } from './task/task.module';
 import { DBUri } from './config';
+import { AuthModule } from './auth/auth.module';
+import { AuthMiddleware } from './middleware/auth.middleware';
 
 @Module({
-  imports: [MongooseModule.forRoot(DBUri), TodoModule, TaskModule],
+  imports: [MongooseModule.forRoot(DBUri), TodoModule, TaskModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes('');
+  }
+}
